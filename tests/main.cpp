89a3374c6c4ce524ac658ad4891a2d63df3f67a8
Evgeny Krutko <e.s.krutko@gmail.com>
2015-04-06 21:53:03 +0300

    init swich to boost test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,34 +1,4 @@
-#include "test_sbfAdditions.h"
-#include "test_sbfNode.h"
-#include "test_sbfElement.h"
-#include "test_sbfMesh.h"
-#include "test_sbfMatrixIterator.h"
-#include "test_stiffMatrixes.h"
-#include "test_sbfWorker.h"
-#include "test_sbfParalleltask.h"
-#include "test_sbfReporter.h"
-
-#include <cstdio>
-
-int main() {
-    QList<QObject*> testObjects;
-
-    testObjects << new TestSbfReporter();
-    testObjects << new TestSbfAdditions();
-    testObjects << new TestSbfNode();
-    testObjects << new TestSbfElement();
-    testObjects << new TestSbfMesh();
-    testObjects << new TestMatrixIterator();
-    testObjects << new TestSbfWorker();
-    testObjects << new TestSbfParallelTask();
-    testObjects << new TestStiffMatrixes();
-
-    int status = 0;
-    for ( auto obj : testObjects )
-        status |= QTest::qExec(obj);
-    if ( status != 0 )
-        std::cout << "Some test failed" << std::endl;
-    else
-        std::cout << "All tests passed :)" << std::endl;
-    return status;
-}
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "Tests for sbf core library"
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
---
+++ tests/sbfStiffMatrixBandIterator.cpp
@@ -0,0 +1,11 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test_log.hpp>
+
+BOOST_AUTO_TEST_SUITE(sbfStiffMatrixBandIterator)
+
+BOOST_AUTO_TEST_CASE(fullMatrix)
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
